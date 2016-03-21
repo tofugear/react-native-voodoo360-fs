9da6e02e9c881832a0f5e800aade963e3e4f9ed9
roku <roku@tofugear.com>
2016-03-21 18:58:10 +0800

    init files

---
+++ android/app/src/main/java/com/voodoo360fs/MainActivity.java
@@ -0,0 +1,45 @@
+package com.voodoo360fs;
+
+import com.facebook.react.ReactActivity;
+import com.facebook.react.ReactPackage;
+import com.facebook.react.shell.MainReactPackage;
+
+import com.rnfs.RNFSPackage; // <------- add package
+import com.tofugear.voodoo360.*;  // <--- import
+
+import java.util.Arrays;
+import java.util.List;
+
+public class MainActivity extends ReactActivity {
+
+    /**
+     * Returns the name of the main component registered from JavaScript.
+     * This is used to schedule rendering of the component.
+     */
+    @Override
+    protected String getMainComponentName() {
+        return "voodoo360fs";
+    }
+
+    /**
+     * Returns whether dev mode should be enabled.
+     * This enables e.g. the dev menu.
+     */
+    @Override
+    protected boolean getUseDeveloperSupport() {
+        return BuildConfig.DEBUG;
+    }
+
+    /**
+     * A list of packages used by the app. If the app uses additional views
+     * or modules besides the default ones, add more packages here.
+     */
+    @Override
+    protected List<ReactPackage> getPackages() {
+        return Arrays.<ReactPackage>asList(
+            new MainReactPackage(), // <---- add comma
+            new RNFSPackage(), // <---------- add package
+            new Voodoo360Package()
+        );
+    }
+}
